4f3a4fed22c25595faa0f22e48bcfb6f52dd785a
Oleksandr Pripa <ol.pripa@gmail.com>
2023-11-07 15:13:21 +0100

    v1

---
+++ ab_console12.py
@@ -0,0 +1,225 @@
+# -*- coding: utf-8 -*-
+""""Console module Adress_Book
+    
+    A simple console module for working with the AdressBook
+    AdressBook --> Name, Phone, E-mail
+    
+    Functions:
+        add Name, phone1, phone2, ..., phoneN: add new user to AdressBook: Name, *Phone
+        load: зчитає з файлу останню збережену адресну книгу
+        phoneadd Name, phone1, phone2, ..., phoneN: add new phone to existing User in AdressBook
+        phonechange Name, phone, newphone: change for existing user (name), phone to newphone
+        phonedel Name, phone: delete phone from list of phone for existing user (name).
+        phone Name: show for existing user (name) list of phone. 
+        show: print to console all items of AdressBook
+        save: збереже поточну адресну книгу у файл, при завершенні роботи спрацьовує автоматично
+        search Str_search: show
+        hello: output info about script
+        . or exit or goodbye or close: exit from script
+
+"""
+from classes import *
+import pickle
+
+
+class MoreArgument(Exception):
+    pass
+
+
+dict_users_phone = AdressBook()
+
+
+def input_error(func):
+    # decorator
+    def inner(*arg):
+        try:
+            result = func(*arg)
+            return result
+        except KeyError:
+            return "User not exist"
+        except ValueError:
+            return 'Give me name and phone please'
+        except IndexError:
+            return 'Enter user name'
+        except TypeError:
+            return 'Arguments is more Type'
+        except MoreArgument:
+            return 'Arguments is more'
+    return inner
+
+
+def hello():
+    # outputs to the console on hello
+    print(__doc__)
+    return f"How can I help you?"
+
+
+# @input_error
+def user_add(name, phones=[]):
+    """
+    Add a new contact to the AdressBook
+    add UserName phone1, phone2, ... phoneN
+    """
+    if name in dict_users_phone.keys():
+        return f'User {name} - exist with phone number {dict_users_phone.get(name).showphones()}'
+    else:
+        r = Record(name)
+        dict_users_phone.add(r)
+        if phones:
+            phone_add(name, phones)
+        return f'add to list {name}, {phones}'
+
+
+# @input_error
+def phone_add(name, phones=[]):
+    # the new phone number of an existing contact
+    if name in dict_users_phone.keys():
+        for ph in phones:
+            try:
+                phone = Phone(ph)
+                print(dict_users_phone.get(name).addphones(phone))
+            except ValueError:
+                return f'Check phone nubmer {ph}'
+
+        return 'add phones completed'  # f'add for {name} phones: {phones}'
+    else:
+        return f'and user {name} not exist'
+
+
+def phone_change(name, phones=[]):
+    # the new phone number of an existing contact
+    if name in dict_users_phone.keys():
+        return dict_users_phone.get(name).editphone(
+            Phone(phones[0]), Phone(phones[1]))
+    else:
+        return f'and user {name} not exist'
+
+
+@input_error
+def phone_del(name, phone):
+    # the new phone number of an existing contact
+    if name in dict_users_phone.keys():
+        return dict_users_phone.get(name).delphone(Phone(phone[0]))
+    else:
+        return f'user {name} not exist'
+
+
+@input_error
+def phone_show(name):
+    # show("username") outputs the phone number for the specified contact to the console.
+    if name in dict_users_phone.keys():
+        return f'User {name} - have phone number {dict_users_phone.get(name).showphones()}'
+    else:
+        return f'and user {name} not exist'
+
+
+@input_error
+def birthday_add(name, birthday):
+
+    if name in dict_users_phone.keys():
+        return dict_users_phone.get(name).addbirthday(birthday[0].strip(' '))
+
+
+@input_error
+def show_all():
+    # outputs all phone number in list to the console.
+    page = Paginator(dict_users_phone)
+    for p in page:
+        result = ''
+        for key in p:
+            result += f'{str(dict_users_phone[key])}\n'
+        print(result)
+        if input('Pres Enter'):
+            continue
+    return f'Done'
+
+
+@input_error
+def search(str_search):
+    if len(str_search) < 3:
+        return f'minimum number of characters to search is 3'
+    else:
+        result = ''
+        for key in dict_users_phone.search(str_search):
+            result += f'{str(dict_users_phone[key])}\n'
+        print(result)
+        return f'Search {str_search}'
+
+
+@input_error
+def load():
+    with open(data_file, "rb") as file:
+        return pickle.load(file)
+
+
+@input_error
+def exit():
+    with open(data_file, "wb") as file:
+        pickle.dump(dict_users_phone, file)
+    print("Good bye!")
+    return
+
+
+data_file = 'data.bin'
+open
+print(data_file)
+
+dict_commands = {"hello": hello,
+                 "add": user_add,
+                 "phoneadd": phone_add,
+                 "phonedel": phone_del,
+                 "phonechange": phone_change,
+                 "phone": phone_show,
+                 "addbirthday": birthday_add,
+                 "show": show_all,
+                 "search": search,
+                 "exit": exit,
+                 "goodbye": exit,
+                 "close": exit,
+                 ".": exit,
+                 }
+
+
+def action(func,  dictionary,  default="Command not exist"):
+    if func in dictionary:
+        return dictionary[func]
+    return lambda * x:  default
+
+
+def parser_arg(text: str):
+    data = text.strip().split(',')
+    if len(data) > 1:
+        name = data.pop(0)
+        t = name, data
+    else:
+        t = data[0],
+    return t
+
+
+def main():
+
+    global dict_users_phone
+
+    dict_users_phone = load()
+    arg = ''
+    print(f'main-code в {__name__} виконується тут\n {__doc__}')
+    try:
+        while True:
+            command, *arg = input('>>>').strip().split(' ', 1)
+            if arg:
+                arg = parser_arg(''.join(arg))
+
+            do = action(command, dict_commands)
+            result = do(*arg)
+            if not result:
+                break
+            print(result)
+
+    except KeyboardInterrupt:
+        exit()
+
+        # print("Good bye!")
+
+
+if __name__ == "__main__":
+    main()
---
+++ classes.py
@@ -0,0 +1,237 @@
+from collections import UserDict
+from collections.abc import Iterator
+from pathlib import Path
+from typing import Any
+from country_phone_dict import country_phone_dict
+from datetime import datetime
+import pickle
+
+
+class Field:
+    def __init__(self, value):
+        self.__value = None
+        self.value = value
+
+    def __str__(self) -> str:
+        return self.value
+
+    def __repr__(self) -> str:
+        return self.value
+
+
+class Name(Field):
+    ...
+
+
+class Phone(Field):
+
+    def __init__(self, value):
+        # super().__init__(value)
+        self.__phone = value
+        self.phone = value
+
+    @property
+    def phone(self):
+        return self.__phone
+
+    @phone.setter
+    # 1. Sanitize phone
+    # 2. Check phone is digits
+    # 3. Chek country cod and lenth
+    def phone(self, value: str):
+        value = self.__sanitize(value)
+        # лічильник, який збільшиться, якщо введений номер введений в правильному форматі (код країни, номер)
+        count = 0
+        if value.isdigit():
+            for cod in country_phone_dict:
+                if value.startswith(cod):
+
+                    if len(value) == country_phone_dict[cod][1]:
+                        self.__phone = value
+                        count = 1
+                        continue
+                    else:
+                        error = 'for {}, the phone number must be long {}'.format(
+                            country_phone_dict[cod][0], country_phone_dict[cod][1])
+                        if not error:
+                            print(error)
+                        raise ValueError(error)
+            if count == 0:
+                raise ValueError('Phone not have country cod')
+
+        else:
+            print('Phone number is not digit')
+            raise ValueError('Phone number is not digit')
+
+    def __eq__(self, other):
+        if hasattr(other, 'phone'):
+            return self.phone == other.phone
+        return self.value == other
+
+    def __str__(self) -> str:
+        return self.phone
+
+    def __repr__(self) -> str:
+        return self.phone
+
+    @classmethod
+    def __sanitize(cls, phone):
+        phone = (
+            phone.strip()
+            .replace("(", "")
+            .replace(")", "")
+            .replace("-", "")
+            .replace("+", "")
+            .replace(" ", "")
+        )
+        return phone
+
+
+class Email(Field):
+    ...
+
+
+class Birthday(Field):
+
+    def __init__(self, value) -> None:
+        self.__birthday = value
+        self.birthday = value
+
+    def __str__(self) -> str:
+        return str(self.__birthday)
+
+    @property
+    def birthday(self):
+        return self.__birthday
+
+    @birthday.setter
+    def birthday(self, value: str):
+        try:
+            self.__birthday = datetime.strptime(value, '%d.%m.%Y').date()
+        except ValueError:
+            print("Format Birthday must be: dd.mm.yyyy")
+            return ValueError("Format Birthday must be: dd.mm.yyyy")
+
+
+class Record(Field):
+
+    def __init__(self, value, phones=None, emails=None, birthday: Birthday = None):
+
+        self.name = Name(value)
+        self.phones: list = phones
+        self.emails: list = emails
+        if birthday:
+            self.birthday = birthday
+        else:
+            self.birthday = None
+
+    def __str__(self) -> str:
+        return 'User: {} {} {} {}'.format(self.name,
+                                          " ".join(
+                                              str(p) for p in self.phones) if self.phones else '',
+                                          " ".join(
+                                              str(e) for e in self.emails) if self.emails else '',
+                                          self.birthday)
+
+    def showphones(self):
+        if self.phones:
+            return 'Phones: {}'.format(", ".join(str(p) for p in self.phones))
+        else:
+            return ''
+
+    def showemails(self):
+        if self.emails:
+            return 'E-mails: {}'.format(", ".join(str(e) for e in self.emails))
+        else:
+            return ''
+
+    def addphones(self, *phones):  # phone --> object class Phone
+        # function  add Phones from Phone list of User
+        if not self.phones:
+            self.phones = [*phones]
+        else:
+            for phone in phones:
+                if phone in self.phones:
+                    return '{} already exiting phone: {}'.format(self.name, phone)
+                else:
+                    self.phones.append(phone)
+                    return 'To list of {} add Phone: {}'.format(self.name, phone)
+
+    def delphone(self, phone: Phone):  # phone --> object class Phone
+        # function  delete Phone from Phone list of User
+
+        if phone in self.phones:
+            return 'from list of {} delete phone: {}'.format(self.name,
+                                                             self.phones.pop(self.phones.index(phone, 0)))
+        else:
+            return '{} not have phone: {}'.format(self.name, phone)
+
+    # function edit Phone from Phone list of User
+    def editphone(self, phone: Phone, new_phone: Phone):  # phone --> object class Phone
+
+        if phone in self.phones:
+            self.phones.insert(self.phones.index(phone, 0)+1, new_phone)
+            return 'in list of {} Phone: {} change to {}'.format(self.name,
+                                                                 self.phones.pop(
+                                                                     self.phones.index(phone, 0)),
+                                                                 new_phone)
+        else:
+            return '{} not have phone: {}'.format(self.name, phone)
+
+    def daystoBirthday(self):
+        if self.birthday:
+            today = datetime.today()
+            bd = datetime(year=today.year, month=self.birthday.birthday.month,
+                          day=self.birthday.birthday.day)
+            if bd < today:
+                bd = bd.replace(year=today.year + 1)
+
+            time_to_birthday = abs(bd - today)
+
+            return (f'!!!! {time_to_birthday.days}')
+
+    def addbirthday(self, birthday: Birthday):
+        if self.birthday == None:
+            self.birthday = birthday
+            return 'To list of {} add: {}'.format(self.name, birthday)
+
+
+class AdressBook(UserDict):
+
+    def __iter__(self) -> Iterator:
+        return iter(self.data)
+
+    def add(self, record: Record):
+        self.update({record.name.value: record})  # .name
+
+    def search(self, search_str: str):
+        keys = []
+        if search_str.isdigit():
+            print('Search Phone and Name')
+            for key, record in self.items():
+                if str(record.name).find(search_str) >= 0 or record.showphones().find(search_str.lower()) >= 0:
+                    keys.append(key)
+            return keys
+
+        else:
+            print('Search Name')
+            for key, record in self.items():
+                if str(record.name).lower().find(search_str.lower()) >= 0:
+                    keys.append(key)
+            return keys
+
+
+class Paginator:
+    def __init__(self, iterable, page_len=3):
+        self.iterable = iterable
+        self.page_len = page_len
+
+    def __iter__(self):
+        page = []
+        for i in self.iterable:
+            page.append(i)
+            if len(page) == self.page_len:
+                yield page
+                page = []
+        if page:
+            yield page
